e2356d522bcd3f0eb3a033c499809ff17938b2d5
Maxime Bégué <begue.maxime@hotmail.fr>
2023-03-11 21:46:58 +0100

    refactored variables

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-let now = Date.now();
-let horaires = [];
-let busLines = [];
+let nextBusObject = {
+  hour: "",
+  line: "",
+};
+let hoursArray = [];
+
 const getNextBusHours = async () => {
   await fetch(
     "http://api.tisseo.fr/v2/stops_schedules.json?key=46cb1cc7-907c-4d9a-a39a-00f4be153433&number=5&stopAreaId=stop_area:SA_1312&dateTime=2023-03-11+17:45"
@@ -11,16 +14,18 @@ const getNextBusHours = async () => {
       departures.forEach((departure) => {
         departure.destination.forEach((terminus) => {
           if (terminus.name === "Basso Cambo") {
-            horaires.push(Date.parse(departure.dateTime));
-            busLines.push(departure.line.shortName);
+            let bus = Object.create(nextBusObject);
+            bus.hour = Date.parse(departure.dateTime);
+            bus.line = departure.line.shortName;
+            hoursArray.push(bus);
           }
         });
       });
-      const nextBus = Math.floor((horaires[0] - now) / 60000);
-      const secondBus = Math.floor((horaires[1] - now) / 60000);
+      const nextBus = Math.floor((hoursArray[0].hour - Date.now()) / 60000);
+      const secondBus = Math.floor((hoursArray[1].hour - Date.now()) / 60000);
 
       console.log(
-        `le prochain bus ${busLines[0]} à destination de Basso Cambo passe dans ${nextBus} minutes, le suivant est un ${busLines[1]} et passe dans ${secondBus} minutes`
+        `le prochain bus ${hoursArray[0].line} à destination de Basso Cambo passe dans ${nextBus} minutes, le suivant est un ${hoursArray[1].line} et passe dans ${secondBus} minutes`
       );
     });
 };
