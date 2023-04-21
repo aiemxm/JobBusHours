6c8af952eac3fbc3c47667b85f006179e49eb2ff
mabegue <maxime.begue@capgemini.com>
2023-04-21 16:05:31 +0200

    modifs

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const dayjs = require('dayjs')
 
 const apiKey = process.env.API_KEY;
 let nextBusObject = {
@@ -9,38 +8,40 @@ let nextBusObject = {
 let hoursArray = [];
 
 const getNextBusHours = async () => {
-  await fetch(
-    `http://api.tisseo.fr/v2/stops_schedules.json?key=${apiKey}&number=5&stopAreaId=stop_area:SA_1312&dateTime=2023-03-11+17:45`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      let departures = data.departures.departure;
-      departures.forEach((departure) => {
-        departure.destination.forEach((terminus) => {
-          if (terminus.name === "Basso Cambo") {
-            let bus = Object.create(nextBusObject);
-            bus.hour = Date.parse(departure.dateTime);
-            bus.line = departure.line.shortName;
-            hoursArray.push(bus);
-          }
-        });
-      });
-      const nextBus = Math.floor((hoursArray[0].hour - Date.now()) / 60000);
-      const secondBus = Math.floor((hoursArray[1].hour - Date.now()) / 60000);
+  try {
+    const response = await fetch(
+      `http://api.tisseo.fr/v2/stops_schedules.json?key=${apiKey}&number=5&stopAreaId=stop_area:SA_1312&dateTime=2023-03-11+17:45`
+    );
+    const data = await response.json();
 
-      const result =  `Prochains bus destination Basso Cambo :
+    let departures = await data.departures.departure;
+    departures.forEach((departure) => {
+      departure.destination.forEach((terminus) => {
+        if (terminus.name === "Basso Cambo") {
+          let bus = Object.create(nextBusObject);
+          bus.hour = Date.parse(departure.dateTime);
+          bus.line = departure.line.shortName;
+          hoursArray.push(bus);
+        }
+      });
+    });
+  } catch (error) {
+    console.log("error" + error);
+  }
+  const nextBus = Math.floor((hoursArray[0].hour - Date.now()) / 60000);
+  const secondBus = Math.floor((hoursArray[1].hour - Date.now()) / 60000);
+
+  const result = `Prochains bus destination Basso Cambo :
       ${hoursArray[0].line} : ${nextBus} mn
       ${hoursArray[1].line} : ${secondBus} mn`;
-      console.log(
-        `Prochains bus destination Basso Cambo :
+  console.log(
+    `Prochains bus destination Basso Cambo :
         ${hoursArray[0].line} : ${nextBus} mn
         ${hoursArray[1].line} : ${secondBus} mn`
-      );
+  );
 
-      return result;
-    });
+  return result;
 };
-
 
 // définir l'heure à laquelle vous souhaitez exécuter la fonction
 const heureExecution = { heures: 17, minutes: 55, secondes: 0 };
@@ -51,25 +52,36 @@ function tempsRestant() {
   const heureActuelle = maintenant.getHours();
   const minuteActuelle = maintenant.getMinutes();
   const secondeActuelle = maintenant.getSeconds();
-  
+
   let tempsEnMs = 0;
-  
-  if (heureActuelle < heureExecution.heures || (heureActuelle === heureExecution.heures && minuteActuelle < heureExecution.minutes) || (heureActuelle === heureExecution.heures && minuteActuelle === heureExecution.minutes && secondeActuelle < heureExecution.secondes)) {
-    tempsEnMs = (heureExecution.heures - heureActuelle) * 3600 * 1000 + (heureExecution.minutes - minuteActuelle) * 60 * 1000 + (heureExecution.secondes - secondeActuelle) * 1000;
+
+  if (
+    heureActuelle < heureExecution.heures ||
+    (heureActuelle === heureExecution.heures &&
+      minuteActuelle < heureExecution.minutes) ||
+    (heureActuelle === heureExecution.heures &&
+      minuteActuelle === heureExecution.minutes &&
+      secondeActuelle < heureExecution.secondes)
+  ) {
+    tempsEnMs =
+      (heureExecution.heures - heureActuelle) * 3600 * 1000 +
+      (heureExecution.minutes - minuteActuelle) * 60 * 1000 +
+      (heureExecution.secondes - secondeActuelle) * 1000;
   } else {
-    tempsEnMs = (24 - heureActuelle + heureExecution.heures) * 3600 * 1000 + (heureExecution.minutes - minuteActuelle) * 60 * 1000 + (heureExecution.secondes - secondeActuelle) * 1000;
+    tempsEnMs =
+      (24 - heureActuelle + heureExecution.heures) * 3600 * 1000 +
+      (heureExecution.minutes - minuteActuelle) * 60 * 1000 +
+      (heureExecution.secondes - secondeActuelle) * 1000;
   }
-  
+
   return tempsEnMs;
 }
-
+getNextBusHours();
 // fonction pour exécuter la fonction et planifier la prochaine exécution
 function executerEtPlanifier() {
-  getNextBusHours();
-  
   // calculer le temps restant jusqu'à l'heure d'exécution suivante
   const tempsEnMs = tempsRestant();
-  
+
   // planifier la prochaine exécution
   setTimeout(executerEtPlanifier, tempsEnMs);
 }
--- scriptable.js
+++ scriptable.js
@@ -1,8 +1,9 @@
 // Script for the Scriptable iOS Application
 
 // Récupération de la clé API depuis les environnements Scriptable
-const apiKey = Keychain.get("tisseo-api-key");
-
+const apiKey = "46cb1cc7-907c-4d9a-a39a-00f4be153433";
+let url =
+  "http://api.tisseo.fr/v2/stops_schedules.json?key=${apiKey}&number=5&stopAreaId=stop_area:SA_1312&dateTime=now";
 // Définition de l'objet pour stocker les horaires des prochains bus
 let nextBusObject = {
   hour: "",
@@ -12,44 +13,26 @@ let nextBusObject = {
 // Tableau pour stocker les horaires des prochains bus
 let hoursArray = [];
 
-// Fonction pour récupérer les horaires des prochains bus en direction de Basso Cambo
-const getNextBusHours = async () => {
-  // Appel à l'API Tisseo pour récupérer les horaires
-  await fetch(
-    `http://api.tisseo.fr/v2/stops_schedules.json?key=${apiKey}&number=5&stopAreaId=stop_area:SA_1312&dateTime=now`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      let departures = data.departures.departure;
-      departures.forEach((departure) => {
-        departure.destination.forEach((terminus) => {
-          if (terminus.name === "Basso Cambo") {
-            let bus = Object.create(nextBusObject);
-            bus.hour = Date.parse(departure.dateTime);
-            bus.line = departure.line.shortName;
-            hoursArray.push(bus);
-          }
-        });
-      });
-      // Calcul des minutes avant l'arrivée des deux prochains bus
-      const nextBus = Math.floor((hoursArray[0].hour - Date.now()) / 60000);
-      const secondBus = Math.floor((hoursArray[1].hour - Date.now()) / 60000);
+// Appel à l'API Tisseo pour récupérer les horaires
+let req = new Request(url);
+let response = await req.json();
 
-      const result = `Prochains bus destination Basso Cambo :
+let departures = data.departures.departure;
+departures.forEach((departure) => {
+  departure.destination.forEach((terminus) => {
+    if (terminus.name === "Basso Cambo") {
+      let bus = Object.create(nextBusObject);
+      bus.hour = Date.parse(departure.dateTime);
+      bus.line = departure.line.shortName;
+      hoursArray.push(bus);
+    }
+  });
+});
+// Calcul des minutes avant l'arrivée des deux prochains bus
+const nextBus = Math.floor((hoursArray[0].hour - Date.now()) / 60000);
+const secondBus = Math.floor((hoursArray[1].hour - Date.now()) / 60000);
+
+const result = `Prochains bus destination Basso Cambo :
         ${hoursArray[0].line} : ${nextBus} mn
         ${hoursArray[1].line} : ${secondBus} mn`;
 
-      // Programmation de la notification pour 17h55
-      let notification = new Notification();
-      notification.title = "Prochains bus Tisséo";
-      notification.body = result;
-      let date = new Date();
-      date.setHours(17);
-      date.setMinutes(55);
-      date.setSeconds(0);
-      notification.schedule = date;
-    });
-};
-
-// Exécution de la fonction pour récupérer les horaires des prochains bus
-getNextBusHours();
